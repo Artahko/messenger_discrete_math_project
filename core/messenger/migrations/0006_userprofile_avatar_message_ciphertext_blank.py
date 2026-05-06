a97bdbd4c3e575f41dd47b60b12f4a91f049d949
Marichka-chicheri <maryfedorenko08@gmail.com>
2026-05-06 17:13:48 +0300

    stabilizing

---
+++ core/messenger/migrations/0006_userprofile_avatar_message_ciphertext_blank.py
@@ -0,0 +1,21 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('messenger', '0005_message_file_message_file_name_message_file_type'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='userprofile',
+            name='avatar',
+            field=models.CharField(max_length=32, default='bob'),
+        ),
+        migrations.AlterField(
+            model_name='message',
+            name='ciphertext',
+            field=models.TextField(blank=True, default=''),
+        ),
+    ]
